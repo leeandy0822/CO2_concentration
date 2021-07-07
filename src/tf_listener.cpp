26759a672d17c975121364e3282b1bcc9dcabe9a
leeandy0822 <leeandy90833@gmail.com>
2021-07-07 17:17:10 +0800

    add moving cube

--- src/tf_listener.cpp
+++ src/tf_listener.cpp
@@ -8,6 +8,7 @@
 #include <tf/LinearMath/Vector3.h>
 #include <geometry_msgs/Transform.h>
 #include <nav_msgs/Odometry.h>
+#include <visualization_msgs/Marker.h>
 
 
 
@@ -16,7 +17,8 @@ using namespace ros;
 
 tf::TransformListener *tf_listener;
 tf::StampedTransform robot_transform;
-
+tf::Quaternion q;
+tf::Vector3 v;
 
 
 void listener(){
@@ -32,30 +34,82 @@ void listener(){
     cout << "Frame id:" << robot_transform.frame_id_ << ", Child id:" << robot_transform.child_frame_id_ << endl;
     double yaw, pitch, roll;
     robot_transform.getBasis().getRPY(roll, pitch, yaw);
-    tf::Quaternion q = robot_transform.getRotation();
-    tf::Vector3 v = robot_transform.getOrigin();
+    q = robot_transform.getRotation();
+    v = robot_transform.getOrigin();
     std::cout << "- Robot Translation: [" << v.getX() << ", " << v.getY() << ", " << v.getZ() << "]" << std::endl;
     std::cout << "- Rotation: in Quaternion [" << q.getX() << ", " << q.getY() << ", "
               << q.getZ() << ", " << q.getW() << "]" << std::endl;
+
+
+    
     }
 
     catch (tf::TransformException& ex)
     {
       std::cout << "Exception thrown:" << ex.what() << std::endl;
     }
+    
 }
+
+
 
 int main(int argc, char** argv){
 
-  ros::init(argc, argv, "robot_tf_listener");
+  ros::init(argc, argv, "co2_concentration");
   ros::NodeHandle n;
+  ros::Rate r(1);
+
+  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  uint32_t shape = visualization_msgs::Marker::CUBE;
 
   tf_listener = new tf::TransformListener();
 
+
   while (ros::ok())
   {
-    ros::spinOnce();
     listener();
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "my_frame";
+    marker.header.stamp = ros::Time::now();
+  
+    marker.ns = "basic_shapes";
+    marker.id = 0;
+
+    marker.type = shape;
+
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.position.x = v.getX();
+    marker.pose.position.y = v.getY();
+    marker.pose.position.z = v.getZ();
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    marker.scale.x = 1.0;
+    marker.scale.y = 1.0;
+    marker.scale.z = 1.0;
+
+    marker.color.r = 1.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration();
+
+    while (marker_pub.getNumSubscribers() < 1)
+    {
+      if (!ros::ok())
+      {
+        return 0;
+      }
+      ROS_WARN_ONCE("Please create a subscriber to the marker");
+      sleep(1);
+    }
+    marker_pub.publish(marker);
+    r.sleep();
+
+
   }
  
 }
